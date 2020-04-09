46d68d29347ae28568ee9d58f09d847c78b6e437
bmeatayi <bmeatayi@gmail.com>
2020-04-09 10:40:29 +0200

    change to two classes

--- examples/ssvep_demo/experiment.py
+++ examples/ssvep_demo/experiment.py
@@ -15,9 +15,9 @@ def main():
     parser.add_argument("-f", "--filename", dest="filename", type=str, help="Record file name")
     args = parser.parse_args()
 
-    target_pos = [(-14, -10), (-14, 10), (14, 10), (14, -10)]
-    hints = [u'\u2199', u'\u2196', u'\u2197', u'\u2198']
-    fr_rates = [5, 6, 7, 8]  # 12hz - 10hz - 8.5hz - 7.5hz
+    target_pos = [(-16, 0), (16, 0)]  # [(-14, -10), (-14, 10), (14, 10), (14, -10)]
+    hints = [u'\u2190', u'\u2192'] # [u'\u2199', u'\u2196', u'\u2197', u'\u2198']
+    fr_rates = [6, 8]  # [5, 6, 7, 8] -> 12hz - 10hz - 8.5hz - 7.5hz
     exp_device = explorepy.Explore()
     exp_device.connect(device_name=args.name)
     exp_device.record_data(file_name=args.filename, file_type='edf')
@@ -28,7 +28,7 @@ def main():
         exp_device.set_marker(code)
 
     experiment = SSVEPExperiment(frame_rates=fr_rates, positions=target_pos, hints=hints, marker_callback=send_marker,
-                                 trial_len=4, trials_per_block=4, n_blocks=10,
+                                 trial_len=6, trials_per_block=5, n_blocks=20,
                                  screen_refresh_rate=60
                                  )
     experiment.run()
